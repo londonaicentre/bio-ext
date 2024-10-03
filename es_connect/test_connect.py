9e5e0f7436893c66ff72865024d1fd8028c7d761
drjzhn <drjzhn@gmail.com>
2024-10-03 13:51:35 +0100

    added search test

--- es_connect/test_connect.py
+++ es_connect/test_connect.py
@@ -14,3 +14,57 @@ indices = es_session.list_indices()
 print("\nElasticsearch Indices:")
 for index in indices:
     print(index)
+
+# test search
+query = {
+    "size": 500,
+    "query": {
+        "bool": {
+            "must": [
+                {"term": {"document_Name.keyword": "Clinical-MDM"}},
+                {"wildcard": {"document_Content": "*breast*"}}
+            ]
+        }
+    }
+}
+
+results = es_session.search(index="gstt_clinical_cancer_documents", body=query)
+
+print("\nSearch Results:")
+print(f"Total: {results['hits']['total']['value']}")
+print(f"Retrieved: {len(results['hits']['hits'])}")
+for hit in results['hits']['hits']:
+    print(f"Document ID: {hit['_id']}")
+    print(f"Score: {hit['_score']}")
+    print(f"Document Name: {hit['_source'].get('document_Name', 'Missing')}")
+    print(f"Content Preview: {hit['_source'].get('document_Content', 'Missing')[:42]}...")
+print("\nRetrieval complete")
+
+# Note that ES results are returned in the following format:
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/search-your-data.html
+# 
+# {
+#   "took": <time in milliseconds>,
+#   "timed_out": <boolean>,
+#   "_shards": { ... },
+#   "hits": { ... }
+# }
+#
+# Where hits is further structured as:
+# "hits": {
+#   "total": { "value": <total_count>, "relation": "eq" },
+#   "max_score": <highest_score>,
+#   "hits": [ ... ]
+# }
+#
+# And nested hits as:
+# {
+#   "_index": "<index_name>",
+#   "_type": "_doc",
+#   "_id": "<document_id>",
+#   "_score": <relevance_score>,
+#   "_source": { ... }
+# }
+#
+# Where "_source" contains the document content.
+#
