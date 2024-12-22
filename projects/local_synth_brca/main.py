eaaa5f227075df3dab8f04c0f5d4b1b53c55dfcd
sophie22 <rzsozso@gmail.com>
2024-12-22 19:44:28 +0000

    move ES functions to base class methods

--- projects/local_synth_brca/main.py
+++ projects/local_synth_brca/main.py
@@ -1,8 +1,6 @@
 import argparse
 import json
 import os
-from synthetic_brca_load_ES import create_index, load_docs_from_file
-from synthetic_brca_retrieve import retrieve_docs
 from synthetic_brca_load_Docc import file2Doc
 from synthetic_brca_stream_Docc import stream_labelled_docs
 from bioext.elastic_utils import ElasticsearchSession
@@ -44,7 +42,7 @@ def parse_CLI_args():  # -> argparse.Namespace:
         default="data/brca_reports.json",
         help="Path to file to load documents from, expected to be JSON",
     )
-    parser_ESl.set_defaults(subcommand="ES_loads")
+    parser_ESl.set_defaults(subcommand="ES_load")
 
     # Parsing command line args for ES_query subcommand
     parser_ESq = subparsers.add_parser("ES_query", help="help")
@@ -73,19 +71,13 @@ def parse_CLI_args():  # -> argparse.Namespace:
     return args
 
 
-def load_es(es_load_cfg, data_file_path):
-    # connect and log on to ElasticSearch
-    print("Connecting to ElasticSearch")
-    es_session = ElasticsearchSession()
-
-    # print(es_load_cfg)
-
+def load_es(es_session, es_load_cfg, data_file_path):
     print("Creating an index...")
-    create_index(es_session.es, es_load_cfg)
+    es_session.create_index(es_load_cfg)
 
     # load json to ElasticSearch
     print("Indexing documents...")
-    successes = load_docs_from_file(es_session.es, data_file_path, es_load_cfg)
+    successes = es_session.load_docs_from_file(data_file_path, es_load_cfg)
     print("Indexed %d/%d documents" % (successes, 100))
 
 
@@ -102,33 +94,37 @@ if __name__ == "__main__":
         assert "ElasticSearch" in app_config.keys()
         assert "load" in app_config["ElasticSearch"].keys()
 
-    if args.subcommand == "ES_load":
-        es_load_cfg = app_config["ElasticSearch"]["load"]
-        load_es(es_load_cfg, args.data)
-        print("Ingestion complete")
-
-    elif args.subcommand == "ES_query":
-        es_query_cfg = app_config["ElasticSearch"]["retrieve"]["breast_brca_query"]
-        os.makedirs(args.output_dir, exist_ok=True)
-
+    if args.subcommand.startswith("ES"):
+        # Initialise a connection to ES server with env credentials
         # connect and log on to ElasticSearch
         print("Connecting to ElasticSearch")
         es_session = ElasticsearchSession()
 
-        # retrieve and save to file the queried documents
-        retrieve_docs(es_session.es, args.output_dir, es_query_cfg)
-        print("Retrieval complete")
+        if args.subcommand == "ES_load":
+            es_load_cfg = app_config["ElasticSearch"]["load"]
+            load_es(es_session, es_load_cfg, args.data)
+            print("Ingestion complete")
 
-    elif args.subcommand == "Doc_load":
-        doc_load_cfg = app_config["Doccano"]["load"]
-        doc_session = DoccanoSession()
-        file2Doc(doc_session, args.data, doc_load_cfg)
+        elif args.subcommand == "ES_query":
+            es_query_cfg = app_config["ElasticSearch"]["retrieve"]["breast_brca_query"]
+            os.makedirs(args.output_dir, exist_ok=True)
 
-        print("Doccano project setup complete")
+            # retrieve and save to file the queried documents
+            es_session.retrieve_docs(args.output_dir, es_query_cfg)
 
-    elif args.subcommand == "Doc_stream":
-        doc_stream_cfg = app_config["Doccano"]["retrieve"]
-        doc_session = DoccanoSession()
-        stream_labelled_docs(doc_session, doc_stream_cfg)
+    elif args.subcommand.startswith("Doc"):
+        # Initialise connection to Doccano
 
-        print("Labelled data streaming complete")
+        if args.subcommand == "Doc_load":
+            doc_load_cfg = app_config["Doccano"]["load"]
+            doc_session = DoccanoSession()
+            file2Doc(doc_session, args.data, doc_load_cfg)
+
+            print("Doccano project setup complete")
+
+        elif args.subcommand == "Doc_stream":
+            doc_stream_cfg = app_config["Doccano"]["retrieve"]
+            doc_session = DoccanoSession()
+            stream_labelled_docs(doc_session, doc_stream_cfg)
+
+            print("Labelled data streaming complete")
--- src/bioext/elastic_utils.py
+++ src/bioext/elastic_utils.py
@@ -1,58 +1,142 @@
 import os
-from elasticsearch import Elasticsearch
+import tqdm
+import json
+from elasticsearch import Elasticsearch, helpers
 from elastic_transport import RequestsHttpNode
 import requests
+
 
 # thanks @LAdams for implementing required http proxy
 class GsttProxyNode(RequestsHttpNode):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.proxy_endpoint = os.getenv("http_proxy")
-        self.session.proxies = {"http": self.proxy_endpoint, "https":self.proxy_endpoint}
+        self.session.proxies = {
+            "http": self.proxy_endpoint,
+            "https": self.proxy_endpoint,
+        }
+
 
 class ElasticsearchSession:
-    def __init__(self, proxy=None, conn_mode:str="HTTP"):
-        requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+    def __init__(self, proxy=None, conn_mode: str = "HTTP"):
+        requests.packages.urllib3.disable_warnings(
+            requests.packages.urllib3.exceptions.InsecureRequestWarning
+        )
 
-        self.es_server = os.getenv("ELASTIC_SERVER", "https://sv-pr-elastic01:9200") # set to GSTT server by default
+        # set to GSTT server by default
+        self.es_server = os.getenv("ELASTIC_SERVER", "https://sv-pr-elastic01:9200")
 
         if proxy:
             self.proxy_node = GsttProxyNode
         else:
             self.proxy_node = None
 
-        try:
-            if conn_mode == "API":
-                self.api_id = os.getenv("ELASTIC_API_ID")
-                self.api_key = os.getenv("ELASTIC_API_KEY")
-                
-                if not all([self.api_id, self.api_key]):
-                    raise ValueError("Check that ELASTIC_API_ID and ELASTIC_API_KEY are in env variables")
+        if conn_mode == "API":
+            self.api_id = os.getenv("ELASTIC_API_ID")
+            self.api_key = os.getenv("ELASTIC_API_KEY")
 
-                self.es = Elasticsearch(
-                    hosts=self.es_server,
-                    api_key=(self.api_id, self.api_key),
-                    node_class=self.proxy_node,
-                    verify_certs=False,
-                    ssl_show_warn=False
+            if not all([self.api_id, self.api_key]):
+                raise ValueError(
+                    "Check that ELASTIC_API_ID and ELASTIC_API_KEY are in env variables"
                 )
 
-            elif conn_mode == "HTTP":
-                self.es_user = os.getenv("ELASTIC_USER")
-                self.es_pwd = os.getenv("ELASTIC_PWD")
-                
-                if not all([self.es_user, self.es_pwd]):
-                    raise ValueError("Check ELASTIC_USER and ELASTIC_PWD are in env variables")
-                        
-                self.es = Elasticsearch(
-                    hosts=self.es_server,
-                    basic_auth=(self.es_user, self.es_pwd),  # http_auth has been deprecated
-                    verify_certs=False,
-                    ssl_show_warn=False
+            self.es = Elasticsearch(
+                hosts=self.es_server,
+                api_key=(self.api_id, self.api_key),
+                node_class=self.proxy_node,
+                verify_certs=False,
+                ssl_show_warn=False,
+            )
+
+        elif conn_mode == "HTTP":
+            self.es_user = os.getenv("ELASTIC_USER")
+            self.es_pwd = os.getenv("ELASTIC_PWD")
+
+            if not all([self.es_user, self.es_pwd]):
+                raise ValueError(
+                    "Check ELASTIC_USER and ELASTIC_PWD are in env variables"
                 )
-                
-        except:
-            raise ValueError("Argument conn_mode must be 'HTTP' or 'API'")    
+
+            self.es = Elasticsearch(
+                hosts=self.es_server,
+                # http_auth has been deprecated
+                basic_auth=(
+                    self.es_user,
+                    self.es_pwd,
+                ),
+                verify_certs=False,
+                ssl_show_warn=False,
+            )
+
+        else:
+            raise ValueError("Argument conn_mode must be 'HTTP' or 'API'")
+
+        # Ensure that connection can actually be made with the creds provided
+        # assert self.es.ping() is True, print(
+        #     "Could not connect with credentials provided"
+        # )
+
+    def create_index(self, config):
+        """Creates an index in ElasticSearch if one isn't already there."""
+
+        self.es.indices.create(
+            index=config["index_name"],
+            body={
+                "settings": {"number_of_shards": 1},
+                "mappings": config["mappings"],
+            },
+            ignore=400,
+        )
 
     def list_indices(self):
         return self.es.indices.get_alias(index="*")
+
+    def _yield_doc(self, data_file_path):
+        """Reads the file through csv.DictReader() and for each row
+        yields a single document. This function is passed into the bulk()
+        helper to create many documents in sequence.
+        """
+        # load json from data file
+        try:
+            with open(data_file_path, "r") as file:
+                data = json.load(file)
+                yield from data
+        except Exception as e:
+            print(f"Failed to load samples: {str(e)}")
+
+    def load_docs_from_file(self, data_file_path, config):
+        progress = tqdm.tqdm(unit=" docs", total=100)
+        successes = 0
+        for ok, action in helpers.streaming_bulk(
+            client=self.es,
+            index=config["index_name"],
+            actions=self._yield_doc(data_file_path),
+        ):
+            progress.update(1)
+            successes += ok
+        return successes
+
+    def retrieve_docs(self, project_dir, query_cfg):
+        index_name = query_cfg["index_name"]
+        query = {"query": query_cfg["query"]}
+        try:
+            results = helpers.scan(
+                client=self.es, query=query, scroll="2m", index=index_name
+            )
+
+            processed_count = 0
+            for hit in results:
+                doc_id = hit["_id"]
+                file_path = os.path.join(project_dir, f"{doc_id}.json")
+
+                with open(file_path, "w") as f:
+                    json.dump(hit, f, indent=2)
+
+                processed_count += 1
+                if processed_count % 1000 == 0:
+                    print(f"Up to {processed_count} docs...")
+
+            print(f"\nTotal: {processed_count} docs")
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
