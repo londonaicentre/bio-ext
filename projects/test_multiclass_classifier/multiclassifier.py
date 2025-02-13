226438b70c0387ed9240c12b4d83af1af1be400f
teddyhla <teddyhla@gmail.com>
2025-02-13 15:15:56 +0000

    phew it works

--- projects/test_multiclass_classifier/multiclassifier.py
+++ projects/test_multiclass_classifier/multiclassifier.py
@@ -1,160 +1,184 @@
-
 import pandas as pd
-import transformers
 import torch
-import datasets
 from pathlib import Path
-from datasets import Dataset, load_dataset, DatasetDict
-from transformers import (AutoTokenizer, AutoModelForSequenceClassification,
-                          PreTrainedModel,BertModel,BertForSequenceClassification,
-                          TrainingArguments,Trainer)
-
-from transformers.modeling_outputs import SequenceClassifierOutput
-
-# TODO : this is need to be checked.
+from datasets import Dataset, DatasetDict
+from transformers import (
+    AutoTokenizer,
+    BertForSequenceClassification,
+    TrainingArguments,
+    Trainer,
+)
 from torch import cuda
-device = 'cuda' if cuda.is_available() else 'cpu'
 
-# LOAD DATA AND ONE HOT ENCODE 
-## task1 label2index ; index2label gen 
+import mlflow
+from mlflow.models import infer_signature
+
+device = "cuda" if cuda.is_available() else "cpu"
+# TODO: refactor from pandas to hugginface datasets
+# TODO: Consider class weighted splitting
 
 DATA_PATH = "data/breast_brca_labelled.json"
-df = pd.read_json(DATA_PATH)
+FRAC = 1
+RANDOM_STATE = 42
+TRAIN_TEST_SPLITPERC = 0.8
+CHOSEN_MODEL = "bert-base-uncased"
+BATCH_SIZE = 8
+SAVELOCATION = "mclassout"
 
-## one-hot encode, conver to FLOAT and concat as list.
-df["label"] = df["label"].astype("category")
-labels = list(df["label"].unique())
+# sense check NEED TO GO TO MLFLOW
+# df["label"].value_counts()
+# df["category"].value_counts()
+# cols = ds["train"].column_names
 
-labelsdict = {key:value for key,value in enumerate(labels)}
-id2label = labelsdict
-label2id = {value:idx for idx,value in id2label.items()}
 
-df["category"] = df["label"].apply(lambda x: label2id[x])
+def load_process_pretokeniser(path):
+    """read the data, generate labelsdict, id2label,label2id
 
-# sense check
-df["label"].value_counts()
-df["category"].value_counts()
-df["category"] = df["category"].astype("int")
+    Args:
+        path (_type_, optional): _description_. Defaults to DATA_PATH.
+    """
+    df = pd.read_json(path)
+    df["label"] = df["label"].astype("category")
+    labels = list(df["label"].unique())
+    num_labels = len(labels)
+    labelsdict = {key: value for key, value in enumerate(labels)}
+    id2label = labelsdict
+    label2id = {value: idx for idx, value in id2label.items()}
+    df["category"] = df["label"].apply(lambda x: label2id[x])
+    df["category"] = df["category"].astype("int")
+    df.columns = ["text", "category", "labels"]
+    print(f"preprocessing complete")
+    return df, labels, num_labels, id2label, label2id
 
-#one_hot = pd.get_dummies(df["label"])
-#df = df.drop("label",axis=1)
-#df = df.join(one_hot)
-#df[labels] = df[labels].astype(float)
 
-df.columns = ["text","category","labels"]
+def create_hfds(data, frac, random_state, splitfrac):
+    """_summary_
 
-# TRAIN TEST SPLIT
-## TODO : consider class weighted splitting.
-df_shuffled = df.sample(frac = 1, random_state= 42).reset_index(drop=True)
-split_index = int(0.8 * len(df_shuffled))
-train_df = df_shuffled.iloc[:split_index]
-test_df = df_shuffled.iloc[split_index:]
+    Args:
+        data (_type_): _description_
+        frac (_type_, optional): _description_. Defaults to FRAC.
+        random_state (_type_, optional): _description_. Defaults to RANDOM_STATE.
+        splitfrac (_type_, optional): _description_. Defaults to TRAIN_TEST_SPLITPERC.
 
-# GENERATE TRANSFORMERS DATASET
-## NOTE: do the column names list concat in transformers as its faster 
-train = Dataset.from_pandas(train_df)
-val = Dataset.from_pandas(test_df)
-ds = DatasetDict()
-ds['train'] = train
-ds['validation'] = val
+    Returns:
+        _type_: _description_
+    """
+    print(
+        f"creating training and testing data by shuffling the {frac} with a random state of {random_state}"
+    )
+    data_shuffled = data.sample(frac=frac, random_state=random_state).reset_index(
+        drop=True
+    )
+    split_index = int(splitfrac * len(data_shuffled))
+    train_df = data_shuffled.iloc[:split_index]
+    test_df = data_shuffled.iloc[split_index:]
 
-## NOTE: remove 
-cols = ds["train"].column_names
-# ds = ds.map(lambda x: {"labels":[x[c] for c in cols if c != "text"]})
+    ds = DatasetDict()
+    train = Dataset.from_pandas(train_df)
+    val = Dataset.from_pandas(test_df)
+    ds["train"] = train
+    ds["validation"] = val
+    print(f"training and testing data generated as Huggingface format")
+    return ds
 
-#id2label = {idx:label for idx,label in enumerate(labels)}
-#label2id = {label:idx for idx,label in enumerate(labels)}
-# ESTABLISH MODEL AND TOKENIZER
-model = "bert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(model)
 
-# note the examples["text"] has to match with the ones from ds 
-def tokenize_encode(examples):
-    return tokenizer(
-        examples["text"],
-        truncation=True,
-        padding="max_length",
-        max_length=tokenizer.model_max_length,
-        return_tensors="pt"
+def prepare_model_and_tokeniser(model_name, numberoflabels, l2i, i2l):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = BertForSequenceClassification.from_pretrained(
+        "bert-base-uncased", num_labels=numberoflabels, label2id=l2i, id2label=i2l
+    )
+    return tokenizer, model
+
+
+def tokenize_ds(ds, tokenizer):
+    def tokenize_encode(examples):
+        return tokenizer(
+            examples["text"],
+            truncation=True,
+            padding="max_length",
+            max_length=512,
+            return_tensors="pt",
         )
 
-#cols.remove("labels")
-ds_enc = ds.map(tokenize_encode,batched=True,remove_columns=["category","text"])
+    ds_enc = ds.map(tokenize_encode, batched=True, remove_columns=["category", "text"])
+    return ds_enc
 
-num_labels = len(labels)
-# now we are tokenised 
-model = BertForSequenceClassification.from_pretrained(
-    "bert-base-uncased", num_labels=5,label2id=label2id,id2label=id2label)
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
-    predictions = torch.argmax(torch.from_numpy(predictions),dim = -1)
+    predictions = torch.argmax(torch.from_numpy(predictions), dim=-1)
     labels = torch.from_numpy(labels)
     accuracy = (predictions == labels).float().mean().item()
     return {"accuracy": accuracy}
 
-batch_size = 8
 
-args = TrainingArguments(
-    output_dir="mclassout",
-    evaluation_strategy = "epoch",
-    learning_rate=2e-5,
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    num_train_epochs=3,
-    weight_decay=0.01,
-    use_cpu=True,
-    no_cuda=True
-)
-
-trainer = Trainer(
+def train_model(
     model,
-    args,
-    train_dataset=ds_enc["train"],
-    eval_dataset=ds_enc["validation"],
-    compute_metrics=compute_metrics,
-    tokenizer=tokenizer)
+    training_dataset,
+    evaluation_dataset,
+    metrics=compute_metrics,
+):
 
-trainer.train()
-trainer.evaluate()
-trainer.save_model("model/")
+    trainingargs = TrainingArguments(
+        output_dir="mclassout",
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        num_train_epochs=3,
+        weight_decay=0.01,
+        use_cpu=True,
+        no_cuda=True,
+    )
 
-def predictor(text,model):
-    #x = [text]
-    encodings = tokenize_encode(text)
-    print("tokenised")
-    with torch.no_grad():
-        outputs = model(**encodings)
-        predictions = torch.nn.functional.softmax(outputs.logits,dim=-1)
-        predicted_label = torch.argmax(predictions,dim = -1)
-    
-    return id2label[predicted_label.item()]
-
-
-text2 = "this is just gibberish is now detected in our samples."
-x = {}
-x["text"] = text2
-tokenize_encode(x)
-prediction = predictor(text = x, model = model)
-
-
-# metrics
-# accuracy[global measure - correct/all], precision and recall for each class
-# confusion matrix
-# macro averaging (equal weight to all class) thats why you average
-# micro averaging (equal weight to each instance ) is where you
-# F1 score 
-# Mattheus Correlation Coefficient. correlation between predicted and actuall. 
-# Cohens kappa 
+    trainer = Trainer(
+        model,
+        args=trainingargs,
+        train_dataset=training_dataset,
+        eval_dataset=evaluation_dataset,
+        compute_metrics=metrics,
+    )
+    trainer.train()
+    return trainer
 
 
-# TODO
-# unknown tokens
-# tokenizer.convert_tokens_to_ids(["VUS"])
-# this code returns a 100
+def main():
+    experiment_name = "bert_multiclass-singlelabel-classification"
+    mlflow.set_tracking_uri("http://localhost:5001")
+    # mlflow.create_experiment(experiment_name)
+    mlflow.set_experiment(experiment_name)
+    print("ML Flow experiement set up ")
 
-example = ds_enc["train"][0]
-tokenizer.decode(example["input_ids"])
-# problem is brca got split
+    df, labels, num_labels, id2label, label2id = load_process_pretokeniser(
+        path=DATA_PATH
+    )
+    print("Data is read, labels dict generated.")
+
+    ds = create_hfds(
+        data=df, frac=FRAC, random_state=RANDOM_STATE, splitfrac=TRAIN_TEST_SPLITPERC
+    )
+    print("Hugginface dataset generated")
+
+    with mlflow.start_run() as run:
+
+        tokenizer, model = prepare_model_and_tokeniser(
+            model_name=CHOSEN_MODEL,
+            numberoflabels=num_labels,
+            l2i=label2id,
+            i2l=id2label,
+        )
+        ds_enc = tokenize_ds(ds=ds, tokenizer=tokenizer)
+
+        trainer = train_model(
+            model=model,
+            training_dataset=ds_enc["train"],
+            evaluation_dataset=ds_enc["validation"],
+            metrics=compute_metrics,
+        )
+
+        trainermetrics = trainer.evaluate()
+        mlflow.log_metrics(trainermetrics)
 
 
+if __name__ == "__main__":
+    main()
