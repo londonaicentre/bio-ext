7203ecbbad6629003af6178e24d680e51f7a303a
Tun Hla <tun.hla@gstt.nhs.uk>
2025-03-26 17:37:09 +0000

    minor errors

--- projects/cogstack_query_dash/pages/3_Searcher.py
+++ projects/cogstack_query_dash/pages/3_Searcher.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 from csdash import dbaccess
 
+load_config = st.cache_data(dbaccess.load_config)
 #
+config = load_config("utils/config_dash.yaml")
 
 # Load existing state objects
 if "csindexes" in st.session_state:
@@ -28,7 +30,7 @@ st.write("You selected:", indexoption)
 
 # Query text
 st.write("Here, input your query string as JSON")
-txt = st.text_area("query", "here is your query")
+txt = st.text_area("query", config["examplequery"])
 
 # Visualise query
 st.markdown("### JSON visualisation of the query")
@@ -36,9 +38,9 @@ st.json(txt,expanded=False)
 
 
 # Visualise result
-st.markdown("### RESULT")
+st.markdown(f"### RESULT {indexoption}")
 df = dbaccess.fetch_query(es=es, query=txt, index_name=indexoption)
-st.write("### SEARCH OUTPUT")
+st.write(f"### SEARCH OUTPUT FROM {indexoption}")
 st.write(f" the size of output: {df.shape}")
 st.write("the data(note size default to 10):")
 st.dataframe(df)
